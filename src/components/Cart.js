3bf21af30f66cdf4fbfe0d3522ee5004add9904b
aecamilleri <aecamilleri@gmail.com>
2023-09-15 00:22:38 -0300

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useCart } from './CartContext';
-import PropGreeting from './PropGreeting';
-
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, updateDoc, getDoc } from 'firebase/firestore';
 
@@ -74,17 +72,21 @@ function Cart() {
       {cart.length === 0 && !purchaseConfirmed ? (
         <div className="text-center">
           <h2 className="text-2xl font-semibold">Tu carrito está vacío</h2>
+          <img src="https://cdn-icons-png.flaticon.com/512/4379/4379542.png" className='w-56 m-auto my-10' alt=''/>
           <p className="text-gray-500 mt-2">¡Ve a comprar algo!</p>
-          <Link to="/" className="mt-4 text-blue-500 hover:underline">Ir a la Tienda</Link>
+          <button className='bg-blue-500 border rounded-md py-2 px-6 my-2.5'>
+            <Link to="/" className="mt-4 text-white hover:underline">Ir a la Tienda</Link>
+          </button>
         </div>
       ) : (
         <>
-          <h2 className="p-8 text-3xl">Tu Carrito de Compras</h2>
-          <PropGreeting greeting="¡Tu Carrito de Compras!" />
           {purchaseConfirmed ? (
             <div className="text-center">
-              <h3 className="text-2xl font-semibold mb-4">¡Compra confirmada!</h3>
-              <Link to="/" className="text-blue-500 hover:underline">Seguir Comprando</Link>
+              <h2 className="text-2xl font-semibold mb-4">¡Compra confirmada!</h2>
+              <img src="https://cdn-icons-png.flaticon.com/512/6569/6569223.png" className='w-56 m-auto my-10' alt=''/>
+              <button className='bg-blue-500 border rounded-md py-2 px-6 my-2.5'>
+                <Link to="/" className="mt-4 text-white hover:underline">Seguir Comprando</Link>
+              </button>
             </div>
           ) : (
             <div className="bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
@@ -111,46 +113,75 @@ function Cart() {
             </div>
           )}
           {!purchaseConfirmed && (
-            <div className="mt-4">
+            <div className="m-6">
               <h3 className="text-xl font-semibold text-end">Total: ${calculateTotalPrice()}</h3>
-              <button
-                onClick={clearCart}
-                className="mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
-              >
-                Vaciar Carrito
-              </button>
-              <Link to="/" className="mt-4 text-blue-500 hover:underline">Seguir Comprando</Link>
-              <form>
-                <div className="mb-4">
-                  <label htmlFor="name" className="block text-gray-600 font-semibold">Nombre:</label>
-                  <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={buyerInfo.name}
-                    onChange={handleInputChange}
-                    className="border rounded-md p-2 w-full"
-                  />
-                </div>
-                <div className="mb-4">
-                  <label htmlFor="email" className="block text-gray-600 font-semibold">Email:</label>
-                  <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={buyerInfo.email}
-                    onChange={handleInputChange}
-                    className="border rounded-md p-2 w-full"
-                  />
-                </div>
+              <div className='flex w-full justify-end'>
                 <button
-                  type="button"
-                  onClick={handleConfirmPurchase}
-                  className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+                  onClick={clearCart}
+                  className="mt-4 bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
                 >
-                  Confirmar Compra
+                  Vaciar Carrito
                 </button>
-              </form>
+                <Link to="/" className="mt-4 text-blue-500 hover:underline ml-4">Seguir Comprando</Link>
+              </div>
+              <div className='p-6'>
+                <form>
+                  <div className="mb-4">
+                  <h3 className="text-xl font-semibold text-black pb-4 border-b-2 border-black">Completa tus Datos</h3>
+                    <label htmlFor="name" className="block text-gray-600 font-semibold pt-4">Nombre:</label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      value={buyerInfo.name}
+                      onChange={handleInputChange}
+                      className="border rounded-md p-2 w-full"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="email" className="block text-gray-600 font-semibold">Email:</label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={buyerInfo.email}
+                      onChange={handleInputChange}
+                      className="border rounded-md p-2 w-full"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="address" className="block text-gray-600 font-semibold">Dirección:</label>
+                    <input
+                      type="text"
+                      id="address"
+                      name="address"
+                      value={buyerInfo.address}
+                      onChange={handleInputChange}
+                      className="border rounded-md p-2 w-full"
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label htmlFor="phone" className="block text-gray-600 font-semibold">Teléfono:</label>
+                    <input
+                      type="tel"
+                      id="phone"
+                      name="phone"
+                      value={buyerInfo.phone}
+                      onChange={handleInputChange}
+                      className="border rounded-md p-2 w-full"
+                    />
+                  </div>
+                  <div className='flex w-full justify-end'>
+                    <button
+                    type="button"
+                    onClick={handleConfirmPurchase}
+                    className="bg-blue-500 text-white py-2 px-4 mt-6 rounded hover:bg-blue-600"
+                    >
+                    Confirmar Compra
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           )}
         </>
